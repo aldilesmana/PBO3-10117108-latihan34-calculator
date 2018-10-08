da7494440c437760b07dca5f7298887527b053f2
aldilesmana <aldilesmana181@gmail.com>
2018-10-09 00:32:22 +0700

    first commit

---
+++ src/main.java
@@ -0,0 +1,33 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+//NAMA  :Aldi Lesmana
+//NIM   :10117108  
+//KELAS :IF-3
+
+/**
+ *
+ * @author Aldi Lesmana
+ */
+public class main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        Kalkulator kal = new Kalkulator();
+
+        System.out.println("====Aplikasi Kalkulator Bilangan====");
+        System.out.print("Masukkan Angka ke-1 : ");
+        kal.value1 = scn.nextDouble();
+        System.out.print("Masukkan Angka ke-2 : ");
+        kal.value2 = scn.nextDouble();
+
+        //format dua digit di belakang koma
+        DecimalFormat df = new DecimalFormat("###.##");
+
+        System.out.println("\nHasil Penjumlahan : "+kal.tambahBilangan());
+        System.out.println("Hasil Pengurangan : "+kal.kurangBiangan());
+        System.out.println("Hasil Perkalian : "+kal.kaliBilangan());
+        System.out.println("Hasil Pembagian : "+df.format(kal.bagiBilangan()));
+        System.out.println("Hasil Sisa : "+kal.sisaBilangan());
+
+    }
+    
+}
